49588675079a4388526194808fdc5ddd38fda674
TG-MB01 <thegym@TG-MB01s-MacBook-Air.local>
2026-03-31 15:21:36 +0200

    refactor: introduce service layer and improve project structure

--- routers/notes.py
+++ routers/notes.py
@@ -1,74 +1,26 @@
-import aiofiles
-import datetime
-import json
-import os
-from fastapi import HTTPException
-from typing import Optional
+from typing import List
 from fastapi import APIRouter
-import asyncio
-from ..schemas import NoteCreate, NoteUpdate, NoteResponse
+from ..schemas import NoteCreate, NoteUpdate
+from ..services import notes_service
 
 router = APIRouter(prefix="/notes", tags=["Notes"])
-data = "data.json"
 
-async def load_notes():
-    exists = await asyncio.to_thread(os.path.exists, data)
-    if not exists:
-        return []
-    async with aiofiles.open(data, "r") as f:
-        content = await f.read()
-        return json.loads(content)
-async def save_notes(notes):
-    async with aiofiles.open(data, "w") as f:
-        await f.write(json.dumps(notes))
-
-@router.get("/")
+@router.get("/", response_model=List[NoteCreate])
 async def get_notes():
-    notes = await load_notes()
-    return {"notes": notes}
+    return await notes_service.get_notes()
 
-@router.get("/get-notes-by-title/")
-async def get_notes_by_title(title: Optional[str]=None, limit: int = 5):
-    notes = await load_notes()
-    filtered_notes = [n for n in notes if n["title"] == title]
-    if not filtered_notes:
-        raise HTTPException(status_code=404, detail="Note not found")
-    return [{"id": n["id"], "title": n["title"], "content": n["content"]} for n in filtered_notes[:limit]]
-
-@router.get("/{note_id}", response_model=NoteResponse)
+@router.get("/{note_id}")
 async def get_note(note_id: int):
- notes = await load_notes()
- new_note = next((note for note in notes if note["id"] == note_id), None)
- if new_note is None:
-     raise HTTPException(status_code=404, detail="Note not found")
- return new_note
+    return await notes_service.get_note(note_id)
 
-@router.post("/", response_model=NoteResponse)
+@router.post("/", response_model=List[NoteCreate])
 async def create_note(note: NoteCreate):
-    notes = await load_notes()
-    new_note_dict = dict(note)
-    new_note_dict["id"] = max((n["id"] for n in notes), default=0) + 1
-    new_note_dict["created_at"] = datetime.datetime.now().isoformat()
-    notes.append(new_note_dict)
-    await save_notes(notes)
-    return new_note_dict
+    return await notes_service.create_note(note)
 
-@router.patch("/{note_id}")
+@router.patch("/{note_id}", response_model=NoteCreate)
 async def update_note(note_id: int, note: NoteUpdate):
-    notes = await load_notes()
-    updated_note = next((n for n in notes if n["id"] == note_id), None)
-    if updated_note is None:
-        raise HTTPException(status_code=404, detail="Note not found")
-    updated_values = note.model_dump(exclude_unset=True)
-    updated_note.update(updated_values)
-    await save_notes(notes)
-    return {"message": f"Note with {note_id} updated successfully!"}
+    return await notes_service.update_note(note_id, note)
 
-@router.delete("/{note_id}")
+@router.delete("/{note_id}", status_code=204)
 async def delete_note(note_id: int):
-    notes = await load_notes()
-    new_notes =  [n for n in notes if n["id"] != note_id]
-    if len(new_notes) == len(notes):
-        raise HTTPException(status_code=404, detail="Notes not found")
-    await save_notes(new_notes)
-    return {"message": f"Note with {note_id} deleted successfully!"}
+    return await notes_service.delete_note(note_id)
--- schemas.py
+++ schemas.py
@@ -3,16 +3,25 @@ from typing import Optional
 from datetime import datetime
 
 class NoteCreate(BaseModel):
+    id: int
     title: str
     content: str
     created_at: Optional[datetime] = datetime.now()
 
+    def update(self, updated_values):
+        pass
+
+
 class NoteUpdate(BaseModel):
     title: Optional[str] = None
     content: Optional[str] = None
+
 
 class NoteResponse(BaseModel):
     id: int
     title: str
     content: str
     created_at: str
+
+class MessageResponse(BaseModel):
+    message: str
